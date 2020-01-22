bb26ed0f56e48c3181baacf2cc3335b8f78888d4
Matt Ryan <matthewtryan99@gmail.com>
2020-01-22 13:35:44 -0600

    Step 2 complete

--- hero_rpg.py
+++ hero_rpg.py
@@ -12,6 +12,11 @@ class Character:
         self.power = power
 
 class Hero(Character):
+    def attack(self, goblin):
+        goblin.health -= hero.power
+        print("You do {} damage to the goblin.".format(hero.power))
+        if goblin.health <= 0:
+            print("The goblin is dead.")
 
 class Goblin(Character):
 
@@ -35,10 +40,7 @@ def main():
         raw_input = input()
         if raw_input == "1":
             # Hero attacks goblin
-            goblin.health -= hero.power
-            print("You do {} damage to the goblin.".format(hero.power))
-            if goblin.health <= 0:
-                print("The goblin is dead.")
+            hero.attack(goblin)
         elif raw_input == "2":
             pass
         elif raw_input == "3":
